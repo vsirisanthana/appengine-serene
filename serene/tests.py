25df06238c7c34a61669d08d206c907beadd5d53
Vichaya Sirisanthana <vsirisanthana@proteus-tech.com>
2013-03-09 14:30:14 +0700

    Initial project setup

---
+++ serene/tests.py
@@ -0,0 +1,12 @@
+__author__ = 'Vichaya Sirisanthana'
+
+
+from unittest2 import TestCase
+
+
+class SimpleTest(TestCase):
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
